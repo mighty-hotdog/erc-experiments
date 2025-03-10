fb1baeec1d230077698a96faeeeaa99811243c41
saracen75 <saracen75@gmail.com>
2025-03-10 15:29:43 +0800

    streamlined state updates to only 3 internal functions

--- src/ERC20Core.sol
+++ src/ERC20Core.sol
@@ -27,11 +27,11 @@ abstract contract ERC20Core is ReentrancyGuard {
     // ERC20 required implementations
     // ****************************************************************************
 
-    // events
+    // events /////////////////////////////////////////////////////////////////////
     /**
      * @notice  Transfer()
      *          Emitted when tokens are transferred, including zero value transfers.
-     * @dev     Also triggered in token creation aka minting, ie: _from == 0x0.
+     * @dev     Also triggered in token creation aka minting, ie: _from == address(0).
      */
     event Transfer(address indexed _from, address indexed _to, uint256 _value);
 
@@ -41,16 +41,16 @@ abstract contract ERC20Core is ReentrancyGuard {
      */
     event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-    // state variables
+    // state variables ////////////////////////////////////////////////////////////
     uint256 private _totalSupply;
     mapping(address => uint256) private _balances;
     mapping(address => mapping(address => uint256)) private _allowances;
 
-    // metadata
+    // metadata ///////////////////////////////////////////////////////////////////
     string private _name;
     string private _symbol;
 
-    // functions
+    // functions //////////////////////////////////////////////////////////////////
     constructor(string memory name_, string memory symbol_) {
         _name = name_;
         _symbol = symbol_;
@@ -77,64 +77,69 @@ abstract contract ERC20Core is ReentrancyGuard {
 
     /**
      * @notice  transfer()
-     *          Transfers tokens from caller to an address and fires Transfer event.
-     * @param   _to     address to transfer to
+     *          Transfers tokens from caller to a recipient address and fires Transfer event.
+     * @param   _to     recipient address to transfer to
      * @param   _value  amount of tokens to transfer
+     *
      * @dev     caller == msg.sender
+     * @dev     _to can be a contract or an EOA, standard doesn't specify
+     *
+     * @dev     reverts if _to == address(0)
      * @dev     reverts if caller balance < _value
      * @dev     _value == 0 allowed and valid
+     * @dev     Reentrancy vulnerability.
+     *          Mitigated by inheriting ReentrancyGuard at contract level and using nonReentrant modifier here
+     *          at function level.
      *
-     * @dev     _to can be a contract or an EOA, standard doesn't specify
-     * @dev     vulnerabilities:
-     *          1. reentrancy
-     *          2. _balances[_to] overflow
+     * @dev     Can be overridden to implement additional logic, eg: more restrictions.
+     *          Any override should also call _updateTokens() or its overridden version to effect the needed
+     *          state changes.
      */
     function transfer(address _to, uint256 _value) public virtual nonReentrant returns (bool success) {
-        if (_value > _balances[msg.sender]) {
-            revert("ERC20: transfer amount exceeds balance");
+        if (_to == address(0)) {
+            revert("ERC20: invalid recipient address(0)");
         }
-        emit Transfer(msg.sender, _to, _value);
-        _balances[msg.sender] -= _value;
-        _balances[_to] += _value;   // just let it revert upon overflow
+        _updateTokens(msg.sender, _to, _value);
         return success;
     }
 
     /**
      * @notice  transferFrom()
-     *          Transfers tokens from source address to destination address and fires Transfer event.
+     *          Transfers tokens from source address to recipient address and fires Transfer event.
      * @param   _from   source address to transfer from
-     * @param   _to     destination address to transfer to
+     * @param   _to     recipient address to transfer to
      * @param   _value  amount of tokens to transfer
      * @dev     Concepts of "allowance", "spender" and "owner":
-     *          An "allowance" is amount of tokens a "spender" is given approval, by an "owner", to spend from owner's balance
-     *          implementation of ERC20 standard must also implement this mechanism.
+     *          An "allowance" is amount of tokens a "spender" is given approval, by an "owner", to spend from
+     *          owner's balance.
+     *          Implementations of ERC20 standard must implement this mechanism.
+     *
      * @dev     caller == msg.sender == "spender"
      * @dev     _from == "owner"
-     * @dev     reverts if allowance == 0
-     *          This could mean spender allowance has been depleted to zero, or that the owner has never approved any allowance.
-     * @dev     reverts if allowance < _value
-     * @dev     reverts if source address balance < _value
-     * @dev     _value == 0 allowed and valid
-     *
      * @dev     _from and _to can be contracts or EOAs, standard doesn't specify
-     * @dev     vulnerabilities:
-     *          1. reentrancy
-     *          2. _balances[_to] overflow
+     *
+     * @dev     reverts if _from == address(0)
+     * @dev     reverts if _to == address(0)
+     * @dev     reverts if _value > allowance
+     * @dev     reverts if _value > source balance
+     * @dev     _value == 0 allowed and valid
+     * @dev     Reentrancy vulnerability.
+     *          Mitigated by inheriting ReentrancyGuard at contract level and using nonReentrant modifier here
+     *          at function level.
+     *
+     * @dev     Can be overridden to implement additional logic, eg: more restrictions.
+     *          Any override should also call _spendAllowance() and _updateTokens() or their overridden versions
+     *          to effect the needed state changes.
      */
-    function transferFrom(address _from, address _to, uint256 _value) public nonReentrant returns (bool success) {
-        if (_value > _balances[_from]) {
-            revert("ERC20: transfer amount exceeds balance");
+    function transferFrom(address _from, address _to, uint256 _value) public virtual nonReentrant returns (bool success) {
+        if (_from == address(0)) {
+            revert("ERC20: invalid source address(0)");
         }
-        if (_allowances[_from][msg.sender] == 0) {
-            revert("ERC20: insufficient allowance");
+        if (_to == address(0)) {
+            revert("ERC20: invalid recipient address(0)");
         }
-        if (_allowances[_from][msg.sender] < _value) {
-            revert("ERC20: insufficient allowance");
-        }
-        emit Transfer(_from, _to, _value);
-        _allowances[_from][msg.sender] -= _value;
-        _balances[_from] -= _value;
-        _balances[_to] += _value;   // just let it revert upon overflow
+        _spendAllowance(_from, msg.sender, _value);
+        _updateTokens(_from, _to, _value);
         return success;
     }
 
@@ -142,22 +147,24 @@ abstract contract ERC20Core is ReentrancyGuard {
      * @notice  approve()
      *          Grants approval to spender to spend tokens from caller balance and fires Approval event.
      * @param   _spender    spender address
-     * @param   _value      allowance, ie: amount of tokens spender is allowed to spend from caller/owner's balance
+     * @param   _value      allowance, ie: amount of tokens spender is allowed to spend from caller's balance
      * @dev     Spender may spend the allowance in single or multiple transactions.
-     * @dev     Calling this function sets the allowance if non exists, or overrides any existing with the new value.
+     * @dev     Calling this function sets the allowance if none exists, or overrides existing with new value.
      *
      * @dev     caller == msg.sender == "owner" in this case
      * @dev     caller and _spender can be a contract or an EOA, standard doesn't specify
-     * @dev     reverts if _value is 0
-     * @dev     vulnerabilities:
-     *          1. reentrancy, ie: calling approve() when transfer() or transferFrom() are being processed
+     *
+     * @dev     reverts if _spender == address(0)
+     * @dev     Reentrancy vulnerability.
+     *          Mitigated by inheriting ReentrancyGuard at contract level and using nonReentrant modifier here at
+     *          function level.
+     *
+     * @dev     Can be overridden to implement additional logic, eg: more restrictions.
+     *          Any override should also call _updateAllowance() or its overridden version to effect the needed
+     *          state changes.
      */
-    function approve(address _spender, uint256 _value) public nonReentrant returns (bool success) {
-        if (_value == 0) {
-            revert("ERC20: approve 0");
-        }
-        emit Approval(msg.sender, _spender, _value);
-        _allowances[msg.sender][_spender] = _value;
+    function approve(address _spender, uint256 _value) public virtual nonReentrant returns (bool success) {
+        _updateAllowance(msg.sender, _spender, _value);
         return success;
     }
 
@@ -170,11 +177,31 @@ abstract contract ERC20Core is ReentrancyGuard {
      * @dev     caller == msg.sender, can be anyone
      * @dev     _owner and _spender can be contracts or EOAs, standard doesn't specify
      */
-    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+    function allowance(address _owner, address _spender) public view virtual returns (uint256 remaining) {
         return _allowances[_owner][_spender];
     }
 
-    // internal functions
+    // internal functions /////////////////////////////////////////////////////////
+    /**
+     * @notice  _updateTokens()
+     *          Effects token transfers, mints and burns by updating balances and totalSupply accordingly.
+     * @param   _from   source address to transfer tokens from
+     * @param   _to     destination address to transfer tokens to
+     * @param   _value  amount of tokens to transfer
+     * @dev     This function is intended to be the only place where token balances and total supply are modified.
+     *          As such, its design is solely concerned with correctly updating balances and total supply, including
+     *          all error checks and reverts needed in performing these operations correctly.
+     *          eg: balance < _value, overflow/underflow
+     * @dev     Application logic, such as address restrictions, allowances, reentrancy, etc. to be handled by higher
+     *          level functions that implement those logic, and then call this function to effect the updates.
+     *
+     * @dev     caller == msg.sender, can be anyone, implementations might like additional restrictions/logic here
+     * @dev     reverts if _value > balance[_from]
+     * @dev     Emits Transfer event as specified in ERC20 standard.
+     *
+     * @dev     Can be overridden to implement additional logic.
+     *          Any override should still emit Transfer event as specified in ERC20 standard.
+     */
     function _updateTokens(address _from, address _to, uint256 _value) internal virtual {
         if ((_from == address(0)) && (_to != address(0))) {
             // minting
@@ -182,14 +209,14 @@ abstract contract ERC20Core is ReentrancyGuard {
             _balances[_to] += _value;
         } else if ((_from != address(0)) && (_to == address(0))) {
             // burning
-            if (_balances[_from] < _value) {
+            if (_value > _balances[_from]) {
                 revert("ERC20: burn amount exceeds balance");
             }
             _totalSupply -= _value;
             _balances[_from] -= _value;
         } else if ((_from != address(0)) && (_to != address(0))) {
             // transfer
-            if (_balances[_from] < _value) {
+            if (_value > _balances[_from]) {
                 revert("ERC20: transfer amount exceeds balance");
             }
             _balances[_from] -= _value;
@@ -197,19 +224,51 @@ abstract contract ERC20Core is ReentrancyGuard {
         }
         emit Transfer(_from, _to, _value);
     }
-    function _updateAllowances(address _owner, address _spender, uint256 _value) internal virtual {
+
+    /**
+     * @notice  _updateAllowance()
+     *          Updates allowance with new value when approve(), and/or similiar functions, are called.
+     * @param   _owner      owner address
+     * @param   _spender    spender address
+     * @param   _value      allowance
+     * @dev     This function is intended to be the only place where allowances are modified due to approvals.
+     *
+     * @dev     caller == msg.sender, can be anyone, implementations might like additional restrictions/logic here
+     *
+     * @dev     reverts if _owner == address(0), to keep that mapping entry "clean"
+     * @dev     reverts if _spender == address(0), to keep that mapping entry "clean"
+     * @dev     Emits Approval event as specified in ERC20 standard.
+     *
+     * @dev     Can be overridden to implement additional logic.
+     *          Any override should still emit Approval event as specified in ERC20 standard.
+     */
+    function _updateAllowance(address _owner, address _spender, uint256 _value) internal virtual {
         if (_owner != address(0)) {
             revert("ERC20: invalid approver address(0)");
         }
         if (_spender != address(0)) {
             revert("ERC20: invalid spender address(0)");
         }
-        if (_value == 0) {
-            revert("ERC20: approve 0");
-        }
         _allowances[_owner][_spender] = _value;
         emit Approval(_owner, _spender, _value);
     }
+
+    /**
+     * @notice  _spendAllowance()
+     *          Updates allowances when spending occurs.
+     * @param   _owner      owner address
+     * @param   _spender    spender address
+     * @param   _value      amount of allowance to spend
+     * @dev     This function is intended to be the only place where allowances are modified due to spending.
+     *
+     * @dev     caller == msg.sender, can be anyone, implementations might like additional restrictions/logic here
+     *
+     * @dev     reverts if _owner == address(0), to keep that mapping entry "clean"
+     * @dev     reverts if _spender == address(0), to keep that mapping entry "clean"
+     * @dev     reverts if _value > allowance
+     *
+     * @dev     Can be overridden to implement additional logic.
+     */
     function _spendAllowance(address _owner, address _spender, uint256 _value) internal virtual {
         if (_owner == address(0)) {
             revert("ERC20: invalid owner address(0)");
@@ -217,10 +276,7 @@ abstract contract ERC20Core is ReentrancyGuard {
         if (_spender == address(0)) {
             revert("ERC20: invalid spender address(0)");
         }
-        if (_allowances[_owner][_spender] == 0) {
-            revert("ERC20: insufficient allowance");
-        }
-        if (_allowances[_owner][_spender] < _value) {
+        if (_value > _allowances[_owner][_spender]) {
             revert("ERC20: insufficient allowance");
         }
         _allowances[_owner][_spender] -= _value;
