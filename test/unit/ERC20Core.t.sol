cc58e697acb0651a29d04ce57f98b1eaf714f9e4
saracen75 <saracen75@gmail.com>
2025-03-10 20:39:31 +0800

    added sample test

---
+++ test/unit/ERC20Core.t.sol
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.28;
+
+import {Test, console} from "forge-std/Test.sol";
+import {ERC20Core} from "../../src/ERC20Core.sol";
+
+contract ERC20CoreTest is Test {
+    function setUp() public {}
+
+    function test_ThisTestDoesNothing() public {
+        console.log("This test does nothing");
+    }
+}
